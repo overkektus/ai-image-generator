e4b0f1e7a82de25d1ac6c5dd2c4894d33d166794
Yahor Piskunou <egor.piskunou@gmail.com>
2023-03-26 15:30:56 +0200

    add Header component

---
+++ components/Header.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import Link from "next/link";
+
+function Header() {
+  return (
+    <header className="flex p-5 justify-between sticky top-0 bg-white z-50 shadow-md">
+      <div className="flex space-x-2 items-center">
+        <Image
+          src="https://links.papareact.com/4t3"
+          alt="logo"
+          height={30}
+          width={30}
+        />
+
+        <div>
+          <h1 className="font-bold">
+            The PAPAFAM <span className="text-violet-500">AI</span> Image
+            Generator
+          </h1>
+          <h2 className="text-xs">
+            Powered by DALL-E, Chat GPT & Microsoft Azure!
+          </h2>
+        </div>
+      </div>
+
+      <div className="flex text-xs md:text-base items-center text-gray-500">
+        <Link href="#" className="px-2 font-light">
+          Github Repo
+        </Link>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
