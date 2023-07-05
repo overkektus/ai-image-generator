85d76b3e6cb306b612c836575304aa10cd4ca996
Yahor Piskunou <egor.piskunou@gmail.com>
2023-07-05 17:27:05 +0200

    add generateImage route

--- components/PromptInput.tsx
+++ components/PromptInput.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import fetchSuggestionFromChatGPT from "@/lib/fetchSuggestionFromChatGPT";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import useSWR from "swr";
 
 function PromptInput() {
@@ -16,11 +16,36 @@ function PromptInput() {
     revalidateOnFocus: false,
   });
 
+  const submitPrompt = async (useSuggestion?: boolean) => {
+    const inputPrompt = input;
+    setInput("");
+
+    const p = useSuggestion ? suggestion : inputPrompt;
+
+    const res = await fetch("/api/generateImage", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ prompt: p }),
+    });
+
+    const data = await res.json();
+  };
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    await submitPrompt();
+  };
+
   const loading = isLoading || isValidating;
 
   return (
     <div className="m-10">
-      <form className="flex flex-col lg:flex-row shadow-md shadow-slate-400/10 border rounded-md lg:divide-x">
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col lg:flex-row shadow-md shadow-slate-400/10 border rounded-md lg:divide-x"
+      >
         <textarea
           value={input}
           onChange={(e) => setInput(e.target.value)}
@@ -43,10 +68,11 @@ function PromptInput() {
           Generate
         </button>
         <button
-          type="button"
           className="p4 bg-violet-400 text-white transition-colors duration-200 font-bold disabled:text-gray-300 disabled:cursor-not disabled:bg-gray-400"
+          type="button"
+          onClick={() => submitPrompt(true)}
         >
-          User Suggestion
+          Use Suggestion
         </button>
         <button
           type="button"
